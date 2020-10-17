3cc4276c6854af5cd00ed2b49e3555ed6c7efb14
tianyu5177 <zhangtianyu5177@sina.com>
2020-10-17 17:05:31 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component,Suspense} from 'react'
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route,Redirect} from 'react-router-dom'
 import Loading from '@/components/Loading'
 import routes from '@/config/routes'
+import '@/assets/css/reset.css'
+import '@/assets/css/main.less'
 
 export default class App extends Component {
 	render() {
@@ -13,6 +15,7 @@ export default class App extends Component {
 							<Route key={route.path} {...route}/>
 						))
 					}
+					<Redirect to="/login"/>
 				</Switch>
 			</Suspense>
 			
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import { NavBar,InputItem,Icon,Button} from 'antd-mobile';
+import './index.less'
 
 export default class Login extends Component {
 	render() {
 		return (
-			<div>
-				Login.....
+			<div className="login-wraper">
+				 {/* 顶部导航区域 */}
+				 <NavBar mode="light">手机验证码登录</NavBar>
+				 {/* 内容区 */}
+				 <div className="content">
+						{/* 手机号输入区 */}
+						<InputItem clear placeholder="请输入手机号">
+							<div className="phone_country">
+								<span>+86</span>
+								<Icon type="down" />
+							</div>
+						</InputItem>
+						{/* 验证码输入区 */}
+						<div className="code">
+							<InputItem clear placeholder="请输六位纯数字验证码"/>
+							<button className="code_btn">获取验证码</button>
+						</div>
+				 </div>
 			</div>
 		)
 	}
