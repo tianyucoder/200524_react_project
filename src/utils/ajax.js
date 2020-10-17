c916fec93d5da4f07ba0eb45d0d1b968a02f5d30
tianyu5177 <zhangtianyu5177@sina.com>
2020-10-17 14:58:14 +0800

    更新

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,12 +1,14 @@
 //该文件是对axios的二次封装，可以：进度条效果、统一修改请求头、统一处理错误等等
 import axios from 'axios'
+//引入NProgress制作请求进度条效果
 import NProgress from 'nprogress'
+//引入nprogress样式
 import 'nprogress/nprogress.css'
 
 //创建一个axios实例
 const ajax = axios.create({
-  baseURL: '/',
-  timeout: 3000,
+  baseURL: '/', //请求公共路径
+  timeout: 3000, //超时时间
 });
 
 //应用请求拦截器
