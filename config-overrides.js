eac2ace52ae8a0a9dea06b0f22aa1bec0aa5ebd6
tianyu5177 <zhangtianyu5177@sina.com>
2020-10-17 15:00:16 +0800

    更新

--- config-overrides.js
+++ config-overrides.js
@@ -7,16 +7,16 @@ const {
 
 module.exports = override(
   fixBabelImports('import', {
-    // libraryName: 'antd',
-    libraryName: 'antd-mobile',
+    // libraryName: 'antd', //antd写这个配置
+    libraryName: 'antd-mobile', //antd-mobile写这个配置
     libraryDirectory: 'es',
     style: true,
 	}),
 	addLessLoader({
 			lessOptions:{
 				javascriptEnabled: true,
-		  	// modifyVars: { '@primary-color': 'orange' },
-		  	modifyVars: { 
+		  	// modifyVars: { '@primary-color': 'orange' }, //antd写这个配置
+		  	modifyVars: { //antd-mobile写这个配置
 					'@brand-primary': '#F40700',
 					'@brand-primary-tap':'#b40600'
 				},
